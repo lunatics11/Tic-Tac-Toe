2603a76071649c54159bc968b86771a80ee412ba
Sarthak Mandpe <sarthakmandpe06@gmail.com>
2026-07-27 13:37:55 +0000

    Initial commit

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -39,25 +39,25 @@ function TicTacToe() {
         if(data[0]===data[1] && data[1]===data[2] && data[2]!==""){
             won(data[2]);
         }
-        else if(data[3]===data[4] && data[4]===data[5] && data[5]!=""){
+        else if(data[3]===data[4] && data[4]===data[5] && data[5]!==""){
             won(data[5]);
         }
-        else if(data[6]===data[7] && data[7]===data[8] && data[8]!=""){
+        else if(data[6]===data[7] && data[7]===data[8] && data[8]!==""){
             won(data[8]);
         }
-        else if(data[0]===data[3] && data[3]===data[6] && data[6]!=""){
+        else if(data[0]===data[3] && data[3]===data[6] && data[6]!==""){
             won(data[6]);
         }
-        else if(data[1]===data[4] && data[4]===data[7] && data[7]!=""){
+        else if(data[1]===data[4] && data[4]===data[7] && data[7]!==""){
             won(data[7]);
         }
-        else if(data[2]===data[5] && data[5]===data[8] && data[8]!=""){
+        else if(data[2]===data[5] && data[5]===data[8] && data[8]!==""){
             won(data[8]);
         }
-        else if(data[0]===data[4] && data[4]===data[8] && data[8]!=""){
+        else if(data[0]===data[4] && data[4]===data[8] && data[8]!==""){
             won(data[8]);
         }
-        else if(data[2]===data[4] && data[4]===data[6] && data[6]!=""){
+        else if(data[2]===data[4] && data[4]===data[6] && data[6]!==""){
             won(data[6]);
         }
     }
@@ -76,7 +76,7 @@ function TicTacToe() {
         setLock(false);
         data=["","","","","","","","",""];
         titleRef.current.innerHTML = 'Tic Tac Toe';
-        box_array.map((e)=>{
+        box_array.forEach((e)=>{
             e.current.innerHTML = "";
         })
     }
